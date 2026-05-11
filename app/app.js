f07056fea86a40973f54d18eb7dbc189537d98b5
Devin Navarre Ali <DEVINNAVARRE@GMAIL.COM>
2026-05-11 02:14:10 -0500

    Automated dashboard sync

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const API_ENDPOINTS = [
-    'https://spinner-helpline-gout.ngrok-free.dev',
     'http://network.alumniinteractive.com:3001',
+    'https://spinner-helpline-gout.ngrok-free.dev',
     'http://10.0.0.99:3001',
     'http://127.0.0.1:3001'
 ];
